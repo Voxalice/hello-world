831e126ad7681d80dbeab642b11474f76e62fff4
Voxalice <64228713+Voxalice@users.noreply.github.com>
2023-03-06 11:34:12 -0600

    add comments

--- autoshopPublic.py
+++ autoshopPublic.py
@@ -3,8 +3,10 @@
 import scratchattach as scratch3
 import requests
 import random
+
 sellerUsername = "USERNAME"
 session = scratch3.login(sellerUsername, "PASSWORD")
+
 conn = session.connect_cloud(801936472)
 client = scratch3.CloudRequests(conn)
 events = scratch3.CloudEvents(669020072)
@@ -12,43 +14,38 @@ events = scratch3.CloudEvents(669020072)
 # Cloud
 
 @client.request
-def p():
+def p(): # Ping command, just to check if this server is online
     return "y"
 
 @client.request
-def s():
+def s(): # Send the contents of shoutout.txt
     with open("./shoutout.txt", "r") as f:
         x = f.read()
         # x += "+" + str(float(requests.get('https://blockbit.yippymishy.repl.co/balance/' + client.get_requester().lower()).json()['balance']))
-        # ^ this line of code returns the viewer's balance, delimited by the "+" character
+        # ^ This line of code adds the "+" character and the viewer's balance 
         return x
 
 @events.event
 def on_set(event):
     if (event.user == "yippymishy"):
+        # BlockBit server has sent a message
+        
         v = scratch3.Encoding.decode(event.value).split("$")
+        
         if (v[0] == "notif"):
-            if (v[1].lower() == sellerUsername):
+            if (v[1].lower() == sellerUsername): # This code runs when this server receives a notification that someone gave @sellerUsername BlockBits
+                
                 print(v)
                 l = open("./autolog.txt", "a")
                 l.write("\n"+str(v))
                 l.close()
-                if (float(v[2]) >= 3):
+                # ^ Log this transaction
+                
+                if (float(v[2]) >= 3): # This code runs when someone gives @sellerUsername 3 BB or more
                     f = open("./shoutout.txt", "w")
                     f.write(v[3])
                     f.close()
-
-                    '''if (float(v[2]) >= 1):
-                    user = session.connect_user(v[3])
-                    proj = user.projects(limit=None, offset=0)
-                    proj = proj[random.randint(0,len(proj)-1)]
-                    proj.love()
-
-                    if (float(v[2]) >= 2):
-                        proj.favorite()
-
-                        if (float(v[2]) >= 4):
-                            user.follow()'''
+                    # ^ Overwrite shoutout.txt with the buyer's username
 
 events.start(thread=True)
 print("Listening for gives")
